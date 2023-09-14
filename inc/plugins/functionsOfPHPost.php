ae6d4d598bf31f109475fc706082e76be18152b8
Miguel92 <joelmiguelvalente@gmail.com>
2023-09-14 09:46:20 -0300

    Cambios 14.09.23:
    *Se eliminó upgrade
    
    * Se editó wysibb.js
    
    * Se hizo un ajuste en functionsOfPHPost.php

--- inc/plugins/functionsOfPHPost.php
+++ inc/plugins/functionsOfPHPost.php
@@ -185,6 +185,7 @@ class fnPHPost {
 		//
 		if(isset($tsUser->uid) OR $tsUser->uid != 0) $data['user_key'] = (int)$tsUser->uid;
 		$data['public'] = $tsCore->settings['public'];
+		$data['pimg'] = $tsCore->settings['public'] . '/images';
 		$data['img'] = $tsCore->settings['images'];
 		$data['url'] = $tsCore->settings['url'];
 		$data['domain'] = $tsCore->settings['domain'];
